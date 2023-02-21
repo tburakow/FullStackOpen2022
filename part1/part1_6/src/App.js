b72ee77762b6ef1576806f1c33cb29533a08d12f
tbdebian <tbdebian@XPS-debian.placeholder.com>
2023-02-21 20:56:04 +0200

    Created button funcs in 1_6

--- part1/part1_6/src/App.js
+++ part1/part1_6/src/App.js
@@ -1,21 +1,41 @@
 import { useState } from 'react'
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
 const App = () => {
     const [ counter, setCounter ] = useState(0)
 
     const increaseByOne = () => setCounter(counter + 1)
-
+    const decreaseByOne = () => setCounter(counter - 1)
     const setToZero = () => setCounter(0)
 
     return (
         <div>
-            <div>{counter}</div>
-            <button onClick={increaseByOne}>
-                plus
-            </button>
-            <button onClick={setToZero}>
-                zero
-            </button>
+            <Display counter={counter} />
+            <Button
+              onClick={increaseByOne}
+              text='plus'
+            />
+            <Button
+              onClick={setToZero}
+              text='zero'
+            />
+            <Button
+              onClick={decreaseByOne}
+              text='minus'
+            />
         </div>
     )
 }
