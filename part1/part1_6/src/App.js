1b0980dffa7a7236dd3782481eec65580dbc9325
tbdebian <tbdebian@XPS-debian.placeholder.com>
2023-02-22 11:23:08 +0200

    refactored 1_6

--- part1/part1_6/src/App.js
+++ part1/part1_6/src/App.js
@@ -1,24 +1,23 @@
 import { useState } from 'react'
 
-const Display = (props) => {
-  return (
-    <div>{props.counter}</div>
-  )
-}
+const Display = ({counter}) => <div>{counter}</div>
 
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 
 const App = () => {
     const [ counter, setCounter ] = useState(0)
+    console.log('rendering with counter value', counter)
 
-    const increaseByOne = () => setCounter(counter + 1)
-    const decreaseByOne = () => setCounter(counter - 1)
+    const increaseByOne = () => {
+      console.log('increasing , value before', counter)
+      setCounter(counter + 1)
+    }
+    const decreaseByOne = () => {
+      console.log('decreasing, value before', counter)
+      setCounter(counter - 1)
+    }
     const setToZero = () => setCounter(0)
 
     return (
