d6d9cc0b0a5e191b23669d937328c920547899f1
tbdebian <tbdebian@XPS-debian.placeholder.com>
2023-02-28 14:35:21 +0200

    Added 2.8

--- 2.6-2.10/src/App.js
+++ 2.6-2.10/src/App.js
@@ -1,39 +1,47 @@
 import { useState } from 'react'
 
 // This component displayes the information of a single person.
-const DisplayPerson = ({ name }) => {
+const DisplayPerson = ({ name, number }) => {
   console.log('person to be displayed (also key):', name)
   return (
-    <p>{name}</p>
+    <p>{name} {number}</p>
   )
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234987' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
       name: newName,
+      number: newNumber,
     }
     if (persons.find(single => single.name === newName) === undefined) {
     setPersons(persons.concat(nameObject))
     console.log(persons)
-    setNewName(' ')
+    setNewName('')
+    setNewNumber('')
     }
     else {
       alert(`${newName} is already in the phonebook`)
     }
     }
-  
 
   return (
     <div>
@@ -43,12 +51,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map(person =>
-        <DisplayPerson name={person.name} key={person.name} />
+        <DisplayPerson name={person.name} number={person.number} key={person.name} />
       )}
     </div>
   )
