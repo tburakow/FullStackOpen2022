25efb9a7c120387e18c8c9a879369243d05646ad
Tuomas Burakowski <burakowskijt@gmail.com>
2023-03-17 14:27:13 +0200

    testing with different url settigns

--- part2_c/src/services/notes.js
+++ part2_c/src/services/notes.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'https://my-awesome-superbackend.onrender.com:3001'
+const baseUrl = 'https://my-awesome-superbackend.onrender.com:3001/api/notes'
+//const baseUrl = 'http://localhost:3001/api/notes/'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
